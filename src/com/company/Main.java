48511a9bb8a7db3306e449accae7e2515923885e
Karina <karinakokosova8@gmail.com>
2019-12-08 23:19:03 +0600

    hw35

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String>listCat=new ArrayList<>();
+        for(int i=0;i<1000001;i++){
+            System.out.println(listCat);
+        }
     }
 }
